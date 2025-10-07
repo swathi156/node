254c780c56076d67c48750e37012408b2f25ed8c
swathi156 <swathiprabhu016@gmail.com>
2025-10-07 12:44:47 +0530

    User.js

--- admin service/models/User.js
+++ admin service/models/User.js
@@ -2,15 +2,10 @@ const { DataTypes } = require("sequelize");
 const sequelize = require("../config/db");
 
 const User = sequelize.define("User", {
-  email: {
-    type: DataTypes.STRING,
-    unique: true,
-    allowNull: false,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
+  email: { type: DataTypes.STRING, allowNull: false, unique: true },
+  password: { type: DataTypes.STRING, allowNull: false },
+  reset_password_token: { type: DataTypes.STRING, allowNull: true },
+  reset_token_expiry: { type: DataTypes.DATE, allowNull: true }
 });
 
 module.exports = User;
