95dbdadad2af84443ebb815b15110b546c049bbb
swathi156 <swathiprabhu016@gmail.com>
2025-10-09 13:45:41 +0530

    doctor.js

--- admin service/models/doctor.js
+++ admin service/models/doctor.js
@@ -1,14 +1,14 @@
 const { DataTypes } = require("sequelize");
-const sequelize = require("../config/db"); 
+const sequelize = require("../config/db");
 
 const Doctor = sequelize.define("Doctor", {
-  name: { type: DataTypes.STRING, allowNull: false },
-  email: { type: DataTypes.STRING, allowNull: false, unique: true },
-  specialization: { type: DataTypes.STRING, allowNull: false },
-  hospital: { type: DataTypes.STRING, allowNull: false },
-  phone: { type: DataTypes.STRING, allowNull: false },
-  password: { type: DataTypes.STRING, allowNull: false },
-  status: { type: DataTypes.STRING, allowNull: false, defaultValue: "active" } 
+    name: { type: DataTypes.STRING, allowNull: false },
+    email: { type: DataTypes.STRING, allowNull: false, unique: true },
+    specialization: { type: DataTypes.STRING },
+    hospital: { type: DataTypes.STRING },
+    phno: { type: DataTypes.STRING },
+    status: { type: DataTypes.STRING },
+    password: { type: DataTypes.STRING }
 });
 
 module.exports = Doctor;
